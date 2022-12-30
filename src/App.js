1107a47464445f37b3e4b6fbea20a0eb71884009
GUchechi <godswilluchendu7@gmail.com>
2022-12-30 17:54:33 +0100

    added data files/api

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Food from './components/Food';
 import HeadlineCards from './components/HeadLineCards';
 import Hero from './components/Hero';
 import Navbar from './components/Navbar';
@@ -9,6 +10,7 @@ function App() {
      <Navbar />
      <Hero />
      <HeadlineCards />
+     <Food />
     </div>
   );
 }
