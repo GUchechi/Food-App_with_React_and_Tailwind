2e0c8f0f4fb67822c1d21589d36551111bffaf49
GUchechi <godswilluchendu7@gmail.com>
2022-12-30 19:05:57 +0100

    category component

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -9,11 +9,11 @@ const Category = () => {
         Top Rated Menu Items
       </h1>
       {/* Categories */}
-      <div className="grid grid-cols-2 md:grid-cols-4 gap-6 py-6">
+      <div className="grid grid-cols-2 md:grid-cols-4 gap-6 py-6 ">
         {categories.map((item, index) => (
           <div
             key={index}
-            className="bg-gray-100 rounded-lg p-4 flex justify-between items-center"
+            className="bg-gray-100 rounded-lg p-4 flex justify-between items-center shadow-2xl"
           >
             <h2 className="font-bold sm:text-xl">{item.name}</h2>
             <img src={item.image} alt={item.name} className="w-20" />
