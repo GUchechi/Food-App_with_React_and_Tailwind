838f1bc02d8cbc7c7552ee40daad89d13e0f773c
GUchechi <godswilluchendu7@gmail.com>
2022-12-30 18:38:24 +0100

    category component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import './App.css';
-import Food from './components/Food';
-import HeadlineCards from './components/HeadLineCards';
-import Hero from './components/Hero';
-import Navbar from './components/Navbar';
+import "./App.css";
+import Category from "./components/Category";
+import Food from "./components/Food";
+import HeadlineCards from "./components/HeadLineCards";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
-     <Navbar />
-     <Hero />
-     <HeadlineCards />
-     <Food />
+      <Navbar />
+      <Hero />
+      <HeadlineCards />
+      <Food />
+      <Category />
     </div>
   );
 }
